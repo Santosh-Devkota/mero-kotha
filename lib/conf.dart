a17c548f030decf289920082b29080f688a6fde6
santosh devkota <devkota549@gmail.com>
2020-09-06 17:19:16 +0545

    updated

--- lib/conf.dart
+++ lib/conf.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 
 import 'main.dart';
 
-class RouteGenerator{
-  static Route<dynamic>  generateRoute(RouteSettings routeSettings){
-    switch(routeSettings.name){
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
       case "/":
-      return MaterialPageRoute(builder: (context)=>HomeScreen());
-      break;
+        return MaterialPageRoute(builder: (context) => HomeScreen());
+        break;
       default:
-      return MaterialPageRoute(builder: (context)=>Container(child: Text("404"),));
+        return MaterialPageRoute(
+            builder: (context) => Container(
+                  child: Text("404"),
+                ));
     }
-
   }
 }
+
+Color baseColor = Color(0xFFF2F2F2);
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
 
 import 'conf.dart';
-void main()=>runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  onGenerateRoute: RouteGenerator.generateRoute,
-  theme: ThemeData(
-    primaryColor: Colors.indigo
-  ),
-  home: HomeScreen(),
-));
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteGenerator.generateRoute,
+      theme: ThemeData(primaryColor: Colors.indigo),
+      home: HomeScreen(),
+    ));
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: AppBar(),
-      
+      body: Home,
     );
   }
 }
