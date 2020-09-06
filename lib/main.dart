e36f26d497e1084ef0965c640d61eb0743929530
santoshacharya <santoshacharya2468@gmail.com>
2020-09-06 16:58:28 +0545

    'neo'

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:clay_containers/clay_containers.dart';
 import 'package:flutter/material.dart';
+import 'package:mero_kotha/widgets/neocontainer.dart';
 
 import 'conf.dart';
 void main()=>runApp(MaterialApp(
@@ -13,8 +15,40 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      appBar: AppBar(),
+      backgroundColor: Colors.grey[300],
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(56),
+        child: NeoContainer(height: 56, width: MediaQuery.of(context).size.width, curveType: null,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+                      IconButton(icon: Icon(Icons.menu),onPressed: (){},)
+
+          ],
+        ),
+        ),
+        
+      ),
+      body: Column(
+        children: [
+          NeoContainer(
+            height:150 ,
+            width: 150,
+            curveType: CurveType.concave,
+          ),
+          Expanded(
+            child:GridView(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,),
+            children: [
+              NeoContainer(height: 150, width: 150, curveType: CurveType.none),
+                NeoContainer(height: 150, width: 150, curveType: CurveType.none),
+                  NeoContainer(height: 150, width: 150, curveType: CurveType.none),
+                    NeoContainer(height: 150, width: 150, curveType: CurveType.none),
+            ],
+            )
+
+          )
+        ],
+      ),
       
     );
   }
