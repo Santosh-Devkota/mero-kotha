4c9b214a02bc3bf070043f2c5c170a379ba63abf
santosh acharya <santoshacharya2468@gmail.com>
2020-11-04 09:18:08 +0545

    'facebook-error-solved''

--- lib/conf.dart
+++ lib/conf.dart
@@ -1,8 +1,4 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mero_kotha/Bloc/authbloc.dart';
-import 'package:mero_kotha/model/department.dart';
 import 'package:mero_kotha/model/facilities.dart';
 import 'package:mero_kotha/pages/contactUsPage.dart';
 import 'package:mero_kotha/pages/errorPage.dart';
@@ -12,9 +8,7 @@ import 'package:mero_kotha/pages/propertyDescription.dart';
 import 'package:mero_kotha/pages/propertySelection.dart';
 import 'package:mero_kotha/pages/register.dart';
 import 'package:mero_kotha/pages/room_search.dart';
-
 import 'main.dart';
-
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
@@ -54,43 +48,12 @@ class RouteGenerator {
   }
 }
 
-// List<Map<String, dynamic>> drawerCategory = [
-//   {
-//     "title": "My Rent",
-//     "icon": Icon(IconData(57360, fontFamily: 'MaterialIcons')),
-//   },
-//   {
-//     "title": "Home",
-//     "icon": Icon(Icons.home),
-//   },
-//   {
-//     "title": "Our Story",
-//     "icon": Icon(Icons.pages),
-//   },
-//   {"title": "Contact Us", "icon": Icon(Icons.phone_in_talk)},
-//   {
-//     "title": "Logout",
-//     "icon": Icon(
-//       AppIcons.login,
-//       size: 20,
-//     ),
-//   },
-// ];
-// Color baseColor = Color(0xFFECF0F3);
-List<Map<String, dynamic>> roomFacilities = [
-  {"title": "wifi", "icon": Icon(Icons.wifi)},
-  {"title": "wifi", "icon": Icon(Icons.wifi)},
-  {"title": "wifi", "icon": Icon(Icons.wifi)},
-  {"title": "wifi", "icon": Icon(Icons.wifi)},
-  {"title": "wifi", "icon": Icon(Icons.wifi)},
-  {"title": "wifi", "icon": Icon(Icons.wifi)}
-];
 Color baseColor = Color(0xFFECF0F3);
 Color backgroundColor = Color(0xFF193566);
 double appBarHeight = 60.0;
 const apiKey = "53465FDSFf##%#%%";
-const baseUrl = "http://45.76.147.177:8080/";
-const imageUrl = "http://45.76.147.177:8080";
+const baseUrl = "http://192.168.1.68:8080/";
+const imageUrl = "http://192.168.1.68:8080";
 
 List<Facilities> listFacilities = [
   Facilities(
@@ -116,32 +79,4 @@ List<Facilities> listFacilities = [
       value: ShutterDirection.East,
       departments: ["shoooter"]),
 ];
-
 enum ShutterDirection { East, West }
-// child: Container(
-//   decoration: BoxDecoration(
-//     border: Border.all(
-//       width: 1,
-//       color: backgroundColor,
-//     ),
-//   ),
-//   padding: EdgeInsets.all(8.0),
-//   width: 300,
-//   height: 50,
-//   child: Row(
-//     mainAxisAlignment: MainAxisAlignment.spaceAround,
-//     children: [
-//       Image.asset(
-//         "assets/icons/google_logo.png",
-//         height: 30,
-//         width: 30,
-//         fit: BoxFit.fill,
-//       ),
-//       Text("Continue with google",
-//           style: Theme.of(context)
-//               .textTheme
-//               .bodyText1
-//               .copyWith(fontSize: 20))
-//     ],
-//   ),
-// ),
